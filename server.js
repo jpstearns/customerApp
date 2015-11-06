5dcd949449ad6ada46ef0dc810771534666c03d0
jpstearns <jared.patrick.stearns@gmail.com>
2015-11-05 19:58:29 -0500

    moify server.js

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.put('/customers/:id', function (req, res) {
   );
 });
 
-app.listen(process.env.PORT);
-console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000);
+console.log("Server running on port 3000...");
